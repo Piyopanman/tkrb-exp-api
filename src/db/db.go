8701fd9378d76c33ef304b37606e5a48194095f9
Piyopanman <g19911kh@gm.tsuda.ac.jp>
2021-03-25 03:08:35 +0900

    DB設定の修正

--- src/db/db.go
+++ src/db/db.go
@@ -1,9 +1,12 @@
 package db
 
 import (
+	"database/sql"
 	"fmt"
+	"os"
 	"tkrb-exp-api/src/logging"
 
+	"github.com/lib/pq"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/gorm/schema"
@@ -14,63 +17,44 @@ var Conn *gorm.DB
 
 //Init データベースの初期化
 func Init(){
-	// err := godotenv.Load()
-	// if err != nil {
-	// 	logging.Logger.Error("Error loading .env file")
-	// }
-	//mysql
-	// user := os.Getenv("MYSQL_USER")
-	// password := os.Getenv("MYSQL_PASSWORD")
-	// // host := os.Getenv("MYSQL_HOST")
-	// host := "mysql"
-	// port := os.Getenv("MYSQL_PORT")
-	// database := os.Getenv("MYSQL_DATABASE")
 
-	// dns := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", user, password, host, port, database)
-	// Conn,err = gorm.Open(mysql.Open(dns), &gorm.Config{
+	//postgreSQL
+	url := os.Getenv("DATABASE_URL")
+	connection,err := pq.ParseURL(url)
+	if err != nil{
+		logging.Logger.Error("Failed to parse db URL")
+		return
+	}
+	connection += " sslmode=require"
+	fmt.Println(connection)
+	sqlDB,err := sql.Open("postgres", url)
+	Conn, err = gorm.Open(postgres.New(postgres.Config{
+		Conn: sqlDB,
+	  }), &gorm.Config{
+		CreateBatchSize: 100,
+			NamingStrategy: schema.NamingStrategy{
+				SingularTable: true,
+			},
+	  })
+	  if err != nil{
+		logging.Logger.Error("Failed to open database")
+		return
+	}
+
+
+	// host := "127.0.0.1"
+	// user := "kakizakihinano"
+	// pass := "ishikari0719"
+	// dbname := "touken_api"
+	// port := "5432"
+	// var err error
+	// dns := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Tokyo", host, user,pass, dbname,port)
+	// 	Conn,err = gorm.Open(postgres.Open(dns), &gorm.Config{
 	// 	CreateBatchSize: 100,
 	// 	NamingStrategy: schema.NamingStrategy{
 	// 		SingularTable: true,
 	// 	},
 	// })
-
-	//postgreSQL
-	// url := os.Getenv("DATABASE_URL")
-	// connection,err := pq.ParseURL(url)
-	// if err != nil{
-	// 	logging.Logger.Error("Failed to parse db URL")
-	// 	return
-	// }
-	// connection += " sslmode=require"
-	// fmt.Println(connection)
-	// sqlDB,err := sql.Open("postgres", url)
-	// Conn, err = gorm.Open(postgres.New(postgres.Config{
-	// 	Conn: sqlDB,
-	//   }), &gorm.Config{
-	// 	CreateBatchSize: 100,
-	// 		NamingStrategy: schema.NamingStrategy{
-	// 			SingularTable: true,
-	// 		},
-	//   })
-	//   if err != nil{
-	// 	logging.Logger.Error("Failed to open database")
-	// 	return
-	// }
-
-
-	host := "127.0.0.1"
-	user := "kakizakihinano"
-	pass := "ishikari0719"
-	dbname := "touken_api"
-	port := "5432"
-	var err error
-	dns := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Tokyo", host, user,pass, dbname,port)
-		Conn,err = gorm.Open(postgres.Open(dns), &gorm.Config{
-		CreateBatchSize: 100,
-		NamingStrategy: schema.NamingStrategy{
-			SingularTable: true,
-		},
-	})
 
 	if err != nil{
 		logging.Logger.Error("Failed to connect to SQL")
