075032c64bb06874f5857964acf0c6f64956a1a4
Piyopanman <g19911kh@gm.tsuda.ac.jp>
2021-03-30 22:58:34 +0900

    DB接続の修正

--- src/db/db.go
+++ src/db/db.go
@@ -2,11 +2,9 @@ package db
 
 import (
 	"database/sql"
-	"fmt"
 	"os"
 	"tkrb-exp-api/src/logging"
 
-	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/gorm/schema"
@@ -20,7 +18,6 @@ func Init(){
 
 	//postgreSQL
 	url := os.Getenv("DATABASE_URL")
-	if(url != ""){
 		logging.Logger.Info("product mode DB")
 		sqlDB,err := sql.Open("postgres", url)
 		Conn, err = gorm.Open(postgres.New(postgres.Config{
@@ -35,30 +32,6 @@ func Init(){
 			logging.Logger.Error("Failed to open database")
 			return
 		}
-	}else{
-		logging.Logger.Info("development mode DB")
-		err := godotenv.Load()
-		if err != nil {
-			logging.Logger.Error("Error loading .env file")
-			return
-		}
-		host := os.Getenv("DB_HOST")
-		user := os.Getenv("DB_USER")
-		pass := os.Getenv("DB_PASSWORD")
-		dbname := os.Getenv("DB_NAME")
-		port := os.Getenv("DB_PORT")
-		dns := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Tokyo", host, user,pass, dbname,port)
-			Conn,err = gorm.Open(postgres.Open(dns), &gorm.Config{
-			CreateBatchSize: 100,
-			NamingStrategy: schema.NamingStrategy{
-				SingularTable: true,
-			},
-		})
-		if err != nil{
-			logging.Logger.Error("Failed to connect to SQL")
-			return
-		}	
-	}
-	logging.Logger.Info("Success to connect to SQL")
+		logging.Logger.Info("Success to connect to SQL")
 	
 }
