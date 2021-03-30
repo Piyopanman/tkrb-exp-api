39264a1b5dc02b6dc28d4b925220a2d8752b2ffb
Piyopanman <g19911kh@gm.tsuda.ac.jp>
2021-03-30 23:02:38 +0900

    DB接続の設定

--- src/db/db.go
+++ src/db/db.go
@@ -2,9 +2,11 @@ package db
 
 import (
 	"database/sql"
+	"fmt"
 	"os"
 	"tkrb-exp-api/src/logging"
 
+	"github.com/lib/pq"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/gorm/schema"
@@ -18,20 +20,32 @@ func Init(){
 
 	//postgreSQL
 	url := os.Getenv("DATABASE_URL")
-		logging.Logger.Info("product mode DB")
-		sqlDB,err := sql.Open("postgres", url)
-		Conn, err = gorm.Open(postgres.New(postgres.Config{
-			Conn: sqlDB,
-		  }), &gorm.Config{
-			CreateBatchSize: 100,
-				NamingStrategy: schema.NamingStrategy{
-					SingularTable: true,
-				},
-		  })
-		if err != nil{
-			logging.Logger.Error("Failed to open database")
-			return
-		}
-		logging.Logger.Info("Success to connect to SQL")
+	connection,err := pq.ParseURL(url)
+	if err != nil{
+		logging.Logger.Error("Failed to parse db URL")
+		return
+	}
+	connection += " sslmode=require"
+	fmt.Println(connection)
+	sqlDB,err := sql.Open("postgres", url)
+	Conn, err = gorm.Open(postgres.New(postgres.Config{
+		Conn: sqlDB,
+	  }), &gorm.Config{
+		CreateBatchSize: 100,
+			NamingStrategy: schema.NamingStrategy{
+				SingularTable: true,
+			},
+	  })
+	  if err != nil{
+		logging.Logger.Error("Failed to open database")
+		return
+	}
+
+	if err != nil{
+		logging.Logger.Error("Failed to connect to SQL")
+		return
+	}
+
+	logging.Logger.Info("Success to connect to SQL")
 	
 }
